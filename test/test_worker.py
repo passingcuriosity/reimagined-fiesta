e186184b192c17138a0dfe13d41f32445be1fdd1
Thomas Sutton <me@thomas-sutton.id.au>
2021-01-02 22:22:08 +1100

    Extract some test fixtures

--- test/test_worker.py
+++ test/test_worker.py
@@ -2,12 +2,7 @@
 
 from datetime import datetime, timedelta
 from queue import Queue
-from unittest.mock import call
 
-import confluent_kafka
-import confluent_kafka.serialization
-import confluent_kafka.schema_registry
-import confluent_kafka.schema_registry.avro
 import pycurl
 import pytest
 from pytest_mock import MockerFixture
@@ -24,12 +19,13 @@ CONFIG = worker.Config(
     auth_cert="/tmp/service.cert"
 )
 
+
 class MockTerminationVariable:
     """Mock variable to signal termination after |n| inspections."""
     def __init__(self, terminate_on: int):
         self.count = terminate_on
         self.reads = 0
-    
+
     @property
     def value(self):
         self.reads += 1
@@ -45,44 +41,16 @@ def test_mock_termination_variable():
     assert terminate.value == 1
 
 
-class Mock:
-    def __init__(self, *args, **kwargs):
-        raise ValueError("ERROR")
-
-def test_worker(mocker: MockerFixture, class_mocker):
-    """Test that the worker performs requests."""
-
-    number_to_process = 3
-    urls = [f"https://localhost/{n + 1}" for n in range(0, number_to_process + 2)]
-
-    start_time = datetime.now()
-    def fake_clock(start_time):
-        fake_time = start_time
-        def now():
-            nonlocal fake_time
-            fake_time += timedelta(seconds=1)
-            return fake_time
-        return now
-
-    mock_datetime_class = class_mocker.patch('latency_logger.worker.datetime', autospec=True)
-    mock_datetime_class.now = fake_clock(start_time)
-    mock_datetime = mock_datetime_class.return_value
-
-    class_mocker.patch('latency_logger.worker.SchemaRegistryClient', autospec=True)
-    class_mocker.patch('latency_logger.worker.AvroSerializer', autospec=True)
-    class_mocker.patch('latency_logger.worker.StringSerializer', autospec=True)
-    
-    producer_class = class_mocker.patch('latency_logger.worker.SerializingProducer', autospec=True)
-    producer = producer_class.return_value
-    producer.poll.return_value = None
-    producer.produce.return_value = None
-
+@pytest.fixture
+def mock_curl(mocker):
+    """Mock [some] PycURL methods."""
     curl_class = mocker.patch('pycurl.Curl', autospec=True)
     curl = curl_class.return_value
     curl.setopt.return_value = None
     curl.perform.return_value = None
 
     n = 0
+
     def fake_info(info, **kwargs):
         nonlocal n
         n += 1
@@ -103,6 +71,50 @@ def test_worker(mocker: MockerFixture, class_mocker):
         return "wot"
     curl.getinfo = fake_info
 
+    return curl
+
+
+@pytest.fixture
+def mock_producer(mocker):
+    """Mock the Kafka producer classes used in worker."""
+    mocker.patch('latency_logger.worker.SchemaRegistryClient', autospec=True)
+    mocker.patch('latency_logger.worker.AvroSerializer', autospec=True)
+    mocker.patch('latency_logger.worker.StringSerializer', autospec=True)
+
+    producer_class = mocker.patch('latency_logger.worker.SerializingProducer', autospec=True)
+    producer = producer_class.return_value
+    producer.poll.return_value = None
+    producer.produce.return_value = None
+
+    return producer
+
+
+@pytest.fixture
+def mock_datetime(mocker):
+    """Mock datetime.datetime in worker."""
+    start_time = datetime.now()
+    fake_time = start_time
+
+    def now():
+        nonlocal fake_time
+        fake_time += timedelta(seconds=1)
+        return fake_time
+
+    mock_cls = mocker.patch('latency_logger.worker.datetime', autospec=True)
+    mock_cls.now = now
+
+    mock_inst = mock_cls.return_value
+    mock_inst.start_time = start_time
+
+    return mock_inst
+
+
+def test_worker(mocker: MockerFixture, mock_curl, mock_producer, mock_datetime):
+    """Test that the worker performs requests."""
+
+    number_to_process = 3
+    urls = [f"https://localhost/{n}" for n in range(0, number_to_process + 2)]
+
     queue = Queue()
     for u in urls:
         queue.put(u)
@@ -112,20 +124,36 @@ def test_worker(mocker: MockerFixture, class_mocker):
 
     assert term.reads == number_to_process + 1
 
-    # Verify calls to cURL methods.
-    actual_requests_processed = len(curl.perform.call_args_list)
-    assert actual_requests_processed == number_to_process
-    actual_urls_requested = [kall.args[1] for kall in curl.setopt.call_args_list if kall.args[0] == pycurl.URL]
+    assert len(mock_curl.perform.call_args_list) == number_to_process
+
+    actual_urls_requested = [
+        c.args[1] for c in mock_curl.setopt.call_args_list if c.args[0] == pycurl.URL
+    ]
     assert actual_urls_requested == urls[0:number_to_process]
 
-    # Verify calls to Producer methods.
-    actual_kafka_polls = len(producer.poll.call_args_list)
-    assert actual_kafka_polls == number_to_process
+    assert len(mock_producer.poll.call_args_list) == number_to_process
 
-    actual_produced_records = [(kall.kwargs['key'], kall.kwargs['value']) for kall in producer.produce.call_args_list]
-    ts = start_time.timestamp()
-    assert actual_produced_records == [
-        ("https://localhost/1", worker.Report("https://localhost/1", code=1, timestamp=ts + 1, namelookup=2, connect=3, appconnect=4, pretransfer=5, starttransfer=6, total=7)),
-        ("https://localhost/2", worker.Report("https://localhost/2", code=8, timestamp=ts + 2, namelookup=9, connect=10, appconnect=11, pretransfer=12, starttransfer=13, total=14)),
-        ("https://localhost/3", worker.Report("https://localhost/3", code=15, timestamp=ts + 3, namelookup=16, connect=17, appconnect=18, pretransfer=19, starttransfer=20, total=21)),
+    ts = mock_datetime.start_time.timestamp()
+
+    def expected_report(n):
+        nonlocal ts
+        t = n * 7
+        return (
+            f"https://localhost/{n}",
+            worker.Report(
+                url=f"https://localhost/{n}",
+                code=t + 1,
+                timestamp=ts + n + 1,
+                namelookup=t + 2,
+                connect=t + 3,
+                appconnect=t + 4,
+                pretransfer=t + 5,
+                starttransfer=t + 6,
+                total=t + 7
+            )
+        )
+
+    actual_produced_records = [
+        (c.kwargs['key'], c.kwargs['value']) for c in mock_producer.produce.call_args_list
     ]
+    assert actual_produced_records == [expected_report(n) for n in range(0, number_to_process)]
